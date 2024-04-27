215c54d77f5a56b5a49c6f3a35a5d0cea49ed23c
leilacsak <leilacsak@gmail.com>
2024-04-27 17:06:58 +0200

    game view and calculatorcontroller added

---
+++ Calculator/Controllers/CalculatorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Calculator.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
