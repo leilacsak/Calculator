d1f4954b8968f6deecaad0c504e5c445e72c34d8
leilacsak <leilacsak@gmail.com>
2024-04-29 20:05:05 +0200

    validations

--- Calculator/Models/CalculatorModel.cs
+++ Calculator/Models/CalculatorModel.cs
@@ -1,14 +1,19 @@
-﻿using System.Globalization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 
 namespace Calculator.Models
 {
     public class CalculatorModel
     {
-        
+        [Required(ErrorMessage = "Number is required")]
+        [RegularExpression(@"^\d+(\.\d+)?$", ErrorMessage = "Your input must be a number")]
         public double Operandus1 { get; set; }
 
+        [Required(ErrorMessage = "Number is required")]
+        [RegularExpression(@"^\d+(\.\d+)?$", ErrorMessage = "Your input must be a number")]
         public double Operandus2 { get;set; }
 
+        [Required(ErrorMessage = "Selecting an operator is required")]
         public String? Operator { get; set; }
         public double Result { get; set; }
     }
